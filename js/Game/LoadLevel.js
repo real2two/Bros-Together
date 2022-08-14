03b9c9b820c6c3da3461b814c70329e23a04ab8c
Two <njsolomonoh@gmail.com>
2022-08-14 10:01:49 -0400

    Update LoadLevel.js

--- js/Game/LoadLevel.js
+++ js/Game/LoadLevel.js
@@ -17,6 +17,7 @@ function loadLevel({ start_pos = { x: 0, y: 0 }, sprites = [], blocks, recording
 
     loadedLevel.start_pos = start_pos;
     loadedLevel.blocks = blocks;
+    loadedLevel.sprites = sprites;
 
     shown_sprites = sprites;
 
